51402e8b71ba8175bbfe539f5a13ba6d632b55df
mellisaputri <76246713+mellisaputri@users.noreply.github.com>
2020-12-19 20:34:41 +0700

    Update mm3.js

--- mm3.js
+++ mm3.js
@@ -1,6 +1,6 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Businessman", "Accounting", "College Student"],
+        strings: ["CEO", "Accounting", "College Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
