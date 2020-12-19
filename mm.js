4ef246a1f27b31bf099c54cdca427b48d8973cc5
mellisaputri <76246713+mellisaputri@users.noreply.github.com>
2020-12-19 17:34:02 +0700

    Add files via upload

---
+++ mm.js
@@ -0,0 +1,7 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Businessman", "Accounting", "College Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
---
+++ mm3.js
@@ -0,0 +1,14 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Businessman", "Accounting", "College Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing2", {
+        strings: ["Seriouly", "I'm Sure About It", "I'm Not Gonna Lie"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
